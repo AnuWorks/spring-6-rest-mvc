e21cccc546d2fda1e7b24348addba332b438f303
Anudeep Madrampalli <anudeep571@gmail.com>
2025-03-29 11:32:44 +1100

    Controller test

---
+++ src/test/java/com/anuworks/spring6restmvc/controller/CustomerControllerTest.java
@@ -0,0 +1,62 @@
+package com.anuworks.spring6restmvc.controller;
+
+import com.anuworks.spring6restmvc.model.Customer;
+import com.anuworks.spring6restmvc.service.CustomerService;
+import com.anuworks.spring6restmvc.service.CustomerServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+import static org.hamcrest.Matchers.is;
+
+@WebMvcTest(CustomerController.class)
+class CustomerControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private CustomerService customerService;
+
+    private final CustomerServiceImpl customerServiceImpl = new CustomerServiceImpl();
+
+    @Test
+    void getCustomerById() throws Exception {
+
+        Customer testCustomer = customerServiceImpl.listCustomers().getFirst();
+
+        given(customerService.getCustomerById(testCustomer.getId())).willReturn(testCustomer);
+
+        mockMvc.perform(get("/api/v1/customer/" + testCustomer.getId()).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", is(testCustomer.getId().toString())));
+
+    }
+
+    @Test
+    void getCustomerList() throws Exception {
+        List<Customer> testCustomers = customerServiceImpl.listCustomers();
+
+        given(customerService.listCustomers()).willReturn(testCustomers);
+
+        mockMvc.perform(get("/api/v1/customer").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.length()", is(3)));
+
+    }
+
+
+}
